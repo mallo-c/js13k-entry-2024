e6265ba419179533e9b6c3e151b786383b3ad288
@mallo_c <simmvad@gmail.com>
2024-09-01 17:04:39 +0500

    add a comment about unreachable()

--- src/unreachable_macro.ts
+++ src/unreachable_macro.ts
@@ -1,3 +1,7 @@
+// This macro is used to mark some place as unreachable.
+// In a debugging environment, this resolves into code that throws an exception.
+// In a production environment, to reduce code size, this resolves only to 0(), causing the browser to throw an exception.
+
 // Because Rust rules
 export default function unreachable /* ! */(msg: string): () => never {
     if (process.env.NODE_ENV == "development")
